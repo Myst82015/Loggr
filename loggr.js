6cd0eb8e08d3a33f57778d1ea972d6e5e6082012
Myst <34489470+Myst82015@users.noreply.github.com>
2021-03-11 10:20:31 +0100

    Update loggr.js

--- loggr.js
+++ loggr.js
@@ -1,1 +1,5 @@
 console.log('Detailed setup instructions and link to code in description: https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+/*
+Joke btw
+Maybe the source code will be released one day.
+*/
